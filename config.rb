5d32fd3dd3d4ef5687a1d14f04c2e3a68fff25f6
tmillot08 <thomas.millot08@gmail.com>
2018-08-28 17:11:06 +0200

    dfcgjbh

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/SimplonThomas/testsass/"
+http_path = "/SimplonThomas/testsass"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
